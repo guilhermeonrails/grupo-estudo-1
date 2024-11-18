56c9be9043fdb75a72dd4da9b773f65dc322c5e2
guilhermelima <guilhermelimaruby@gmail.com>
2024-11-18 15:27:48 -0300

    exemplo-imersao-grupo-de-estudo-1

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const musicas = [
+    {id: 1, nome: "Endless Sacrifice"},
+    {id: 2, nome: "Castle of Glass"},
+    {id: 3, nome: "Like a stone"}
+];
+
+console.log(musicas);
